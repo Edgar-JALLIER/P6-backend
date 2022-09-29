6632a8c2628f06ff1e8a357043af3a57c11b903f
Edgar JALLIER <edgarjallier@gmail.com>
2022-09-29 19:37:14 +0200

    Déplacement du fichier multer

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -3,13 +3,12 @@ const auth = require("../middleware/auth");
 const router = express.Router();
 const sauceCtrl = require("../controllers/sauce");
 // Pour l'enregistrement de nouvelles images dans la BDD
-const multer = require("../images/multer-config");
+const multer = require("../middleware/multer-config");
 
 //On met l'authentification avant le multer pour que la route soit bien sécurisé avant les potentiels changements
 router.post("/", auth, multer, sauceCtrl.createSauce);
 // Utilisation de put pour la mise à jour / modification des données
 // Utilisation de ":id" pour indiquer que c'est un élément dynamique
-
 router.put("/:id", auth, multer, sauceCtrl.modifySauce);
 router.get("/", auth, sauceCtrl.getAllSauce);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
